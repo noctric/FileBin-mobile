28d0c04a1fa450efcabe4188c609bc1acafc9e64
Michael <michael@soapsurfer.de>
2018-04-06 14:49:44 -0300

    forgot to add dir

---
+++ app/src/main/java/de/michael/filebinmobile/fragments/SettingsFragment.java
@@ -0,0 +1,25 @@
+package de.michael.filebinmobile.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.michael.filebinmobile.R;
+
+public class SettingsFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.server_settings_fragment, container, false);
+    }
+}
