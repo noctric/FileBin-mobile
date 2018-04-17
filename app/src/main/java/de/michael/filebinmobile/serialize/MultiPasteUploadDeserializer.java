0cfe6ab740253c2fef7f8f41988043042e417806
Michael <michael@soapsurfer.de>
2018-04-17 20:10:40 -0300

    implemented deserializer for multipastes

--- app/src/main/java/de/michael/filebinmobile/serialize/MultiPasteUploadDeserializer.java
+++ app/src/main/java/de/michael/filebinmobile/serialize/MultiPasteUploadDeserializer.java
@@ -1,7 +1,9 @@
 package de.michael.filebinmobile.serialize;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 import java.lang.reflect.Type;
@@ -12,8 +14,23 @@ public class MultiPasteUploadDeserializer implements com.google.gson.JsonDeseria
 
     @Override
     public MultiPasteUpload deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        
-        return null;
+
+        JsonObject multiPasteJsonItem = json.getAsJsonObject();
+
+        String url_id = multiPasteJsonItem.get("url_id").getAsString();
+        long date = multiPasteJsonItem.get("date").getAsLong();
+
+        JsonArray singleUploadItems = multiPasteJsonItem.get(url_id).getAsJsonObject()
+                .getAsJsonArray("items");
+
+        String[] itemIds = new String[singleUploadItems.size()];
+
+        for (int i = 0; i < singleUploadItems.size(); i++) {
+            // TODO hmmmm
+            itemIds[i] = singleUploadItems.get(i).getAsString();
+        }
+
+        return new MultiPasteUpload(url_id, date, itemIds);
     }
 
 }
