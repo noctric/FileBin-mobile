4de50912af444aeaa33f6bef513f13ccc22d4791
michael <michael.calvert@thoughtworks.com>
2018-12-26 13:32:26 +0100

    updated project structure, bumped some versions and added "lost" icons

--- app/src/main/java/de/michael/filebinmobile/view/GridViewItemDecorator.kt
+++ app/src/main/java/de/michael/filebinmobile/view/GridViewItemDecorator.kt
@@ -11,57 +11,53 @@ import android.view.View
 class GridViewItemDecorator(val gridSpacingPx: Int, val gridSize: Int) : RecyclerView.ItemDecoration() {
     var mNeedLeftSpacing = false
 
-    override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
 
-        if (parent != null && view != null && outRect != null) {
-
-            val frameWidth = ((parent.width - gridSpacingPx * (gridSize - 1)) / gridSize)
-            val padding = parent.width / gridSize - frameWidth
-            val itemPosition = (view.getLayoutParams() as RecyclerView.LayoutParams).viewAdapterPosition
-            if (itemPosition < gridSize) {
-                outRect.top = 0
-            } else {
-                outRect.top = gridSpacingPx
-            }
-            if (itemPosition % gridSize == 0) {
-                outRect.left = 0
-                outRect.right = padding
-                mNeedLeftSpacing = true
-            } else if ((itemPosition + 1) % gridSize == 0) {
-                mNeedLeftSpacing = false
-                outRect.right = 0
-                outRect.left = padding
-            } else if (mNeedLeftSpacing) {
-                mNeedLeftSpacing = false
-                outRect.left = gridSpacingPx - padding
-                if ((itemPosition + 2) % gridSize == 0) {
-                    outRect.right = gridSpacingPx - padding
-                } else {
-                    outRect.right = gridSpacingPx / 2
-                }
-            } else if ((itemPosition + 2) % gridSize == 0) {
-                mNeedLeftSpacing = false
-                outRect.left = gridSpacingPx / 2
+        val frameWidth = ((parent.width - gridSpacingPx * (gridSize - 1)) / gridSize)
+        val padding = parent.width / gridSize - frameWidth
+        val itemPosition = (view.getLayoutParams() as RecyclerView.LayoutParams).viewAdapterPosition
+        if (itemPosition < gridSize) {
+            outRect.top = 0
+        } else {
+            outRect.top = gridSpacingPx
+        }
+        if (itemPosition % gridSize == 0) {
+            outRect.left = 0
+            outRect.right = padding
+            mNeedLeftSpacing = true
+        } else if ((itemPosition + 1) % gridSize == 0) {
+            mNeedLeftSpacing = false
+            outRect.right = 0
+            outRect.left = padding
+        } else if (mNeedLeftSpacing) {
+            mNeedLeftSpacing = false
+            outRect.left = gridSpacingPx - padding
+            if ((itemPosition + 2) % gridSize == 0) {
                 outRect.right = gridSpacingPx - padding
             } else {
-                mNeedLeftSpacing = false
-                outRect.left = gridSpacingPx / 2
                 outRect.right = gridSpacingPx / 2
             }
-            outRect.bottom = 0
+        } else if ((itemPosition + 2) % gridSize == 0) {
+            mNeedLeftSpacing = false
+            outRect.left = gridSpacingPx / 2
+            outRect.right = gridSpacingPx - padding
         } else {
-            return super.getItemOffsets(outRect, view, parent, state)
+            mNeedLeftSpacing = false
+            outRect.left = gridSpacingPx / 2
+            outRect.right = gridSpacingPx / 2
         }
+        outRect.bottom = 0
     }
 }
 
 /**
  * https://stackoverflow.com/a/29720777
  */
-class BottomOffsetDecorator(private val offset: Int, private val condition: () -> Boolean) : RecyclerView.ItemDecoration() {
-    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State?) {
+class BottomOffsetDecorator(private val offset: Int, private val condition: () -> Boolean) :
+    RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
-        val dataSize = state!!.itemCount
+        val dataSize = state.itemCount
         val position = parent.getChildAdapterPosition(view)
         if (condition() && position == dataSize - 1) {
             outRect.set(0, 0, 0, offset)
