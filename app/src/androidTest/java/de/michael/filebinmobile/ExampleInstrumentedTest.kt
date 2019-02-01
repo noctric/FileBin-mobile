b48b06f0d04644bd8f5e77e9d09c23ad8a176ad4
michael <michael.calvert@thoughtworks.com>
2019-02-01 18:49:59 +0100

    add first instrumentation test as an example

--- app/src/androidTest/java/de/michael/filebinmobile/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/michael/filebinmobile/ExampleInstrumentedTest.kt
@@ -1,8 +1,13 @@
 package de.michael.filebinmobile
 
 import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,10 +18,20 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("de.michael.filebinmobile", appContext.packageName)
+    fun shouldShowServerListView() {
+        val resources = InstrumentationRegistry.getContext().resources
+
+        onView(withText("Welcome!")).check(matches(isDisplayed()))
+
+        onView(withText("Yes, take me there!")).perform(click())
+
+        onView(withId(R.id.server_settings_fragment_root_id))
+            .check(matches(isDisplayed()))
+
     }
 }
---
+++ app/src/test/java/de/michael/filebinmobile/controller/ApplicationManagerTest.kt
@@ -0,0 +1,4 @@
+package de.michael.filebinmobile.controller
+
+class ApplicationManagerTest {
+}
